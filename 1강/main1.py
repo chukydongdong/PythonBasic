361f5694a7ec523e65b061dcaee35ab1eac71a21
chukydongdong <95954810+chukydongdong@users.noreply.github.com>
2021-12-11 16:53:38 +0900

    Create main1.py

---
+++ 1강/main1.py
@@ -0,0 +1,69 @@
+print("Hello World")
+print('Hello World')
+print('철수가 인사한다. "안녕?" ')
+print("철수가 인사한다. \"안녕?\"")
+print('나의 \'첫 코딩\' 입니다')
+print("나의 '첫 코딩' 입니다")
+print ("오늘은", "일요일")
+a = 0o177
+print(a)
+a = 0x1
+a = 9999
+b = 1
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+a = 2
+b = 15
+print(a ** b)
+a = 7
+b = 3
+print(a % b)
+print(b % a)
+a = 7
+b = 4
+print(a / b)
+print(a // b)
+a = 80
+b = 75
+c = 65
+print((a+b+c)/3)
+d = a + b + c
+print("총점 :", d)
+print("평균점수 :", d/3)
+a = 89
+b = 3
+print(a // b)
+print(a % b)
+print("나는 김동현입니다 그래서 저는 고래 코딩을 다닙니다")
+print(23 % 2) # 1
+print( 1003 % 3) # 1
+print("안녕하세요. 오늘의 날씨는 맑음 입니다")
+print("지나가던 한 사람이 물었다")
+print(' "실례합니다. 여의도역으로 가는길이 어디입니까?" ')
+print("질문 받은 사람이 대답했다")
+print(' "I do not know" ')
+print(" \\    /\\")
+print("  )  ( ')")
+print(" (  /  )")
+print("  \\(_)|")
+# 1
+A = 14
+B = 3
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A//B)
+print(A%B)
+# 2
+a = 263
+b = 38
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
