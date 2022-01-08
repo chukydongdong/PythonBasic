c0c99595ab6955c124733883c9ce3ec40d38fcb8
chukydongdong <95954810+chukydongdong@users.noreply.github.com>
2022-01-08 16:52:16 +0900

    Create main.py

---
+++ 3강/main.py
@@ -0,0 +1,73 @@
+def 시리얼먹기(우유,시리얼,그릇,숟가락):
+    # print("준비물을 준비함니다.")
+    print("{}을 {}안에 넣습니다.".format(시리얼,그릇))
+    print("{}을 {}이 담긴 {}에 따릅니다.".format(우유,시리얼,그릇))
+    print("{}으로 {}과 {}를 떠서 먹습니다.".format(숟가락,시리얼,우유))
+
+시리얼먹기("연한우유","얼얼한시리얼","화분","뚫어뻥")
+def hello():
+    print("안녕하세요")
+
+hello()
+def 시리얼먹기(우유,시리얼,그릇,숟가락):
+    # print("준비물을 준비함니다.")
+    print("{}을 {}안에 넣습니다.".format(시리얼,그릇))
+    print("{}을 {}이 담긴 {}에 따릅니다.".format(우유,시리얼,그릇))
+    print("{}으로 {}과 {}를 떠서 먹습니다.".format(숟가락,시리얼,우유))
+    return "{} 1리터, {} 500그램 남았습니다.".format(우유,시리얼)
+    
+결과 = 시리얼먹기("연한우유","얼얼한시리얼","화분","뚫어뻥")
+print(결과)
+
+def say1(name):
+    string = '안녕하세요? ' + name + '님'
+    return string
+
+def say2(name):
+    string ='안녕하세요? ' + name + '님'
+    print(string)
+
+name = "홍길동"
+say1(name)
+say2(name)
+
+def add(a, b)
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b): 
+    return a * b
+    
+def div(a,b):
+    return a // b
+    
+a = 6
+b = 3
+print(add(b, a)) # 9
+print(sub(b, a)) # 3
+print(mul(b, a)) # 18
+print(div(b, a)) # 2
+
+def 브라우니만들기(버터,초콜릿,믹스넛,바닐라,달걀):
+    print("초콜릿을 냄비에 넣고 녹인다")
+    print("버터를 잘게 잘라서 냄비에 넣고 녹인다")
+    print("그릇에 달걀을 풀어 놓는다")
+    print("달걀을 푼 그릇에 녹인 초콜릿과 바닐라를 넣고 잘 섞는다 오븐그릇에 담은 다음 믹스넛을 고르게 뿌린다")
+    print("오븐에 180도로 25-30분 굽는다")
+    return 만든브라우니갯수
+
+나의브라우니 = 브라우니만들기(250, 2, 100, 50, 3)
+
+def hello():
+    print("'안녕,함수!'")
+
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
+hello()
