72e07a5d552a68d1d436fcd35a211949fcfcad8d
chukydongdong <95954810+chukydongdong@users.noreply.github.com>
2022-02-05 16:51:00 +0900

    Create main1.py

---
+++ 7강/main1.py
@@ -0,0 +1,74 @@
+print('com1과 com2의 합을 맞추어라')
+print(com1)
+print(com2)
+count = 0
+while True :
+    print('0~20000까지의 숫자를 입력하세요')
+    count = count + 1
+    print("{0}번째 도전 !!!!!".format(count))
+    user = int(input('당신의 선택은 ?'))
+    print(user)
+    if count > 10 :
+            print('도전초과정답은{}임 이걸 못 맞추네 ㅋㅋㄹㅃㅃ'.format(com1+com2))
+            break
+    if user > com1 + com2:
+        print('정답보다 큼')
+    elif user == -1 :
+        print(' 정답은{}임 이걸 못 맞추네 ㅋㅋㄹㅃㅃ'.format(com1+com2))
+    elif user < com1 + com2:
+        print('정답보다 작음')
+    else:
+        print('정답!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        breakprint('com1과 com2의 합을 맞추어라')
+print(com1)
+print(com2)
+count = 0
+while True :
+    print('0~20000까지의 숫자를 입력하세요')
+    count = count + 1
+    print("{0}번째 도전 !!!!!".format(count))
+    user = int(input('당신의 선택은 ?'))
+    print(user)
+    if count > 10 :
+            print('도전초과정답은{}임 이걸 못 맞추네 ㅋㅋㄹㅃㅃ'.format(com1+com2))
+            break
+    if user > com1 + com2:
+        print('정답보다 큼')
+    elif user == -1 :
+        print(' 정답은{}임 이걸 못 맞추네 ㅋㅋㄹㅃㅃ'.format(com1+com2))
+    elif user < com1 + com2:
+        print('정답보다 작음')
+    else:
+        print('정답!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        breakprint('com1과 com2의 합을 맞추어라')
+print(com1)
+print(com2)
+count = 0
+while True :
+    print('0~20000까지의 숫자를 입력하세요')
+    count = count + 1
+    print("{0}번째 도전 !!!!!".format(count))
+    user = int(input('당신의 선택은 ?'))
+    print(user)
+    if count > 10 :
+            print('도전초과정답은{}임 이걸 못 맞추네 ㅋㅋㄹㅃㅃ'.format(com1+com2))
+            break
+    if user > com1 + com2:
+        print('정답보다 큼')
+    elif user == -1 :
+        print(' 정답은{}임 이걸 못 맞추네 ㅋㅋㄹㅃㅃ'.format(com1+com2))
+    elif user < com1 + com2:
+        print('정답보다 작음')
+    else:
+        print('정답!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        break
+    
+
+
+
+
+
+
+
+
+
