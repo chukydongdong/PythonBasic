f5b96af101afdd7b4784100499cafe4aed700546
chukydongdong <95954810+chukydongdong@users.noreply.github.com>
2022-02-26 16:49:20 +0900

    Create main1.py

---
+++ 10강/main1.py
@@ -0,0 +1,20 @@
+점수들 = [90,25,67,45,80,1,100,2,99,3,98]
+
+number = 0
+for 점수 in 점수들:
+       number = number +1
+       if 점수 >= 60:
+              print("{}번 학새은 합격입니다.".format(number))
+       else:
+              print("{}번 학생은 불합격입니다.".format(number))
+
+숫자들 = [10,20,30]
+for 숫자 in 숫자들:
+    print(숫자)
+    print("_______")
+
+동물들 = ['강아지','고양이','앵무새']
+for 동물 in 동물들:
+    print(동물[0])
+
+
